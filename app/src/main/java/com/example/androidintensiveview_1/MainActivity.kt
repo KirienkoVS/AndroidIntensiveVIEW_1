3058d4162f35f9b4a30a5e06cbec0cbfa62d78e2
KirienkoVS <kirienkov@yandex.ru>
2022-02-21 23:21:28 +0300

    Loads image using Glide library

--- app/src/main/java/com/example/androidintensiveview_1/MainActivity.kt
+++ app/src/main/java/com/example/androidintensiveview_1/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.androidintensiveview_1
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -10,14 +8,13 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import java.net.URL
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import java.util.concurrent.Executors
 
 private const val IMAGE_URL = "https://picsum.photos/200/300"
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var image: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +32,9 @@ class MainActivity : AppCompatActivity() {
                 KeyEvent.KEYCODE_ENTER -> {
                     executor.execute {
                         try {
-                            loadImage(editTextView, handler, imageView)
+                            handler.post {
+                                loadImage(editTextView, imageView)
+                            }
                         } catch (e: Exception) {
                             handler.post {
                                 Toast.makeText(this, "${e.message}", Toast.LENGTH_LONG).show()
@@ -49,11 +48,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun loadImage(editTextView: EditText, handler: Handler, imageView: ImageView) {
-        val stream = URL(editTextView.text.toString()).openStream()
-        image = BitmapFactory.decodeStream(stream)
-        handler.post {
-            imageView.setImageBitmap(image)
-        }
+    private fun loadImage(editTextView: EditText, imageView: ImageView) {
+        Glide.with(this)
+            .load(editTextView.text.toString())
+            .override(600, 800)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .into(imageView)
     }
 }
